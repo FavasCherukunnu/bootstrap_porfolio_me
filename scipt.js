f0d7681c721d9940b4dffd60f147224e5c269d28
unknown <mhdfavascheru@gmail.com>
2023-02-25 22:39:31 +0530

    changed top bar

--- scipt.js
+++ scipt.js
@@ -1,43 +1,67 @@
 var topContentDivHeigth;
+var topNamePassionHeight;
+
 
 function loadtopContentDivHeight(){
-    topContentDivHeigth = document.getElementById('rowTop').offsetHeight;
+    topContentDivHeigth = document.getElementById('topBackground').offsetHeight;
     topDivOperations();
+}
+
+function loadTopNamePassionHeight(){
+    topNamePassionHeight = document.getElementById('nameDiv').offsetHeight;
+
 }
 
 function topDivOperations(){
 
-    var topTextHeight = document.getElementById('nameDiv').offsetHeight;
+    var topTextHeight = document.getElementById('iconDiv').offsetHeight;
     var scrollHeight = topContentDivHeigth - window.pageYOffset;
-    // var percent = (scrollHeight/topContentDivHeigth)*100; //find percentage
-
+    // if (window.pageYOffset > 0) {
+    var bottom = document.getElementById('topBackground').offsetHeight;
+    var opac = (window.pageYOffset/bottom)+0.3;
+    // console.log('heigth is', topContentDivHeigth);
+    document.getElementById('background-profile').style.background = "linear-gradient(rgba(0, 0, 0, " + opac + "), rgba(0, 0, 0, " + opac + ")), url(assets/image/background.jpg) no-repeat";
+    document.getElementById('passionDivText').style.color = 'rgba(252, 252, 252,'+ ((1-opac)+0.3) + ')';
+    document.getElementById('nameDivText').style.color = 'rgba(252, 252, 252,'+ ((1-opac)+0.3) + ')';
+    // }
     if(scrollHeight>topTextHeight){
         document.getElementById('topContentDiv').style.height = scrollHeight+'px';
-        document.getElementById('topContentDiv').style.backgroundColor = 'rgba(0, 0, 0, 0)';
-        document.getElementById('passionDiv').style.display = 'block';
-        document.getElementById('passionDivText').style.fontSize = '3vw';
+        document.getElementById('topContentDiv').style.backgroundColor = 'rgba(0, 0, 0, 0)';    //transparent
+        if(topNamePassionHeight>=scrollHeight){
+            document.getElementById('nameDiv').style.display = 'none';
+        }else{
+            document.getElementById('nameDiv').style.display = 'block';
+        }
+        // document.getElementById('passionDiv').style.display = 'block';
+        // document.getElementById('passionDivText').style.fontSize = '3vw';
     }else{
         document.getElementById('topContentDiv').style.backgroundColor = 'black';
         document.getElementById('topContentDiv').style.height = topTextHeight+'px';
-        document.getElementById('passionDivText').style.fontSize = '0';
-        // document.getElementById('passionDiv').style.display = 'none';
+        document.getElementById('nameDiv').style.display = 'none';
+        // document.getElementById('passionDivText').style.fontSize = '0';
     }
+
 
 }
 
 
-document.getElementById('topContentDiv').onload = loadtopContentDivHeight();
-window.addEventListener('resize',loadtopContentDivHeight);
+// document.getElementById('topContentDiv').onload(()=>{
+
+// });
+
+window.addEventListener("load",()=>{
+    loadtopContentDivHeight();
+    loadTopNamePassionHeight();
+});
+window.addEventListener('resize',()=>{
+    loadtopContentDivHeight();
+    loadTopNamePassionHeight();
+
+});
 
 
 window.onscroll = function() {
     
-    if (window.pageYOffset > 0) {
-        var bottom = document.getElementById('rowTop').offsetHeight;
-        var opac = (window.pageYOffset/bottom)+0.3;
-        // console.log('heigth is', topContentDivHeigth);
-        document.getElementById('background-profile').style.background = "linear-gradient(rgba(0, 0, 0, " + opac + "), rgba(0, 0, 0, " + opac + ")), url(assets/image/background.jpg) no-repeat";
-        document.getElementById('passionDivText').style.color = 'rgba(252, 252, 252,'+ ((1-opac)+0.3) + ')';
-    }
+
     topDivOperations();
   }
